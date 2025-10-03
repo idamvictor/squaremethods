199806597af842ac4285d0183aebb384188fa49c
Victor Idam <idamvictorx1@gmail.com>
2025-10-03 08:01:50 +0100

    fix

--- app/(user)/teams/[teamId]/page.tsx
+++ app/(user)/teams/[teamId]/page.tsx
@@ -1,17 +1,3 @@
-// import { TeamDetails } from "@/components/user/teams/team-details";
-
-// export default function TeamDetailsPage({
-//   params,
-// }: {
-//   params: { teamId: string };
-// }) {
-//   return (
-//     <div className="min-h-screen bg-gray-50">
-//       <TeamDetails teamId={params.teamId} />
-//     </div>
-//   );
-// }
-
 import { TeamDetails } from "@/components/user/teams/team-details";
 
 export default async function TeamDetailsPage({
