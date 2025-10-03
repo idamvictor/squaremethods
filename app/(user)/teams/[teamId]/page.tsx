c0a0286894ec972a00fe518dbafcf33aee81c061
Victor Idam <idamvictorx1@gmail.com>
2025-10-03 08:01:19 +0100

    fixed route params bug

--- app/(user)/teams/[teamId]/page.tsx
+++ app/(user)/teams/[teamId]/page.tsx
@@ -1,13 +1,29 @@
+// import { TeamDetails } from "@/components/user/teams/team-details";
+
+// export default function TeamDetailsPage({
+//   params,
+// }: {
+//   params: { teamId: string };
+// }) {
+//   return (
+//     <div className="min-h-screen bg-gray-50">
+//       <TeamDetails teamId={params.teamId} />
+//     </div>
+//   );
+// }
+
 import { TeamDetails } from "@/components/user/teams/team-details";
 
-export default function TeamDetailsPage({
+export default async function TeamDetailsPage({
   params,
 }: {
-  params: { teamId: string };
+  params: Promise<{ teamId: string }>;
 }) {
+  const { teamId } = await params;
+
   return (
     <div className="min-h-screen bg-gray-50">
-      <TeamDetails teamId={params.teamId} />
+      <TeamDetails teamId={teamId} />
     </div>
   );
 }
