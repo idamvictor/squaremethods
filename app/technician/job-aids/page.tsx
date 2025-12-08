f270fb2f5773a25ed2b489e730aba903aff1d150
Victor Idam <idamvictorx1@gmail.com>
2025-12-08 09:53:03 +0100

    made the create job aid reusable

--- app/technician/job-aids/page.tsx
+++ app/technician/job-aids/page.tsx
@@ -1,13 +1,9 @@
-import { JobAidsManagement } from "@/components/user/job-aids/job-aids-management";
+import JobAidsPageContent from "@/components/user/job-aids/job-aids-page-content";
 
 export default function JobAidsPage() {
-  const handleCreateClick = () => {
-    // TODO: Implement create job aid functionality
-  };
-
   return (
     <div className="min-h-screen bg-gray-50">
-      <JobAidsManagement onCreateClick={handleCreateClick} />
+      <JobAidsPageContent />;
     </div>
   );
 }
